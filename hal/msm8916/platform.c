b50fbfe91e3c0e9980227606f3d071d9d91c4b0a
Vince1171 <franchomme.vincent@gmail.com>
2021-02-14 13:29:19 +0100

    audio-hal: further 64 bit hal porting changes

--- hal/msm8916/platform.c
+++ hal/msm8916/platform.c
@@ -1532,7 +1532,7 @@ int platform_set_voice_volume(void *platform, int volume)
               __func__, mixer_ctl_name);
         return -EINVAL;
     }
-    ALOGV("Setting voice volume index: %d", set_values[0]);
+    ALOGV("Setting voice volume index: %ld", set_values[0]);
     mixer_ctl_set_array(ctl, set_values, ARRAY_SIZE(set_values));
 
     if (my_data->csd != NULL) {
